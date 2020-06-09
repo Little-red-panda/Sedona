a697394b725324daa2203c65501d81d569afc219
Little-red-panda <60811698+Little-red-panda@users.noreply.github.com>
2020-06-09 15:18:52 +0300

    Завершение попапа

--- popup.js
+++ popup.js
@@ -4,14 +4,27 @@ let minusAdult = document.querySelector(".minus-adult");
 let plusAdult = document.querySelector(".plus-adult");
 let minusChild = document.querySelector(".minus-child");
 let plusChild = document.querySelector(".plus-child");
+let adultInput = document.querySelector(".adult-input");
+let childInput = document.querySelector(".child-input");
 
 button.addEventListener("click", function(evt) {
     evt.preventDefault();
     popup.classList.toggle("modal-hidden");
 });
 
-// plusAdult.addEventListener("click", function() {
+plusAdult.addEventListener("click", function() {
+    adultInput.value++;
+});
 
-// });
+minusAdult.addEventListener("click", function() {
+    adultInput.value--;
+});
 
+plusChild.addEventListener("click", function() {
+    childInput.value++;
+});
+
+minusChild.addEventListener("click", function() {
+    childInput.value--;
+});
 
